d26c151ea1eaab6a4af655d58c2fc38f71b8937f
Matias Valenti <51561485+MattValenti11@users.noreply.github.com>
2024-09-09 23:53:06 -0300

    Arreglo en componente Proyect

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import '../styles/Projects.css'; // Importa el archivo de estilos
 import dopefolioImage from '../img/dopefolio.jpeg';
+import wilsonportImage from '../img/wilsonport.jpeg'; // Asegúrate de que esta ruta es correcta
 
 const Projects = () => {
+  const handleButtonClick = () => {
+    window.open('http://localhost:3000/', '_blank'); // Abre la URL en una nueva pestaña
+  };
+
   return (
-    <section id="projects" className="projects-container">
-      <div className="projects-image-container">
-        <img src={dopefolioImage} alt="Descripción del proyecto" className="projects-image" />
+    <section id="projects">
+      {/* Primer proyecto - Imagen a la izquierda */}
+      <div className="project-container left">
+        <div className="project-image-container">
+          <img src={dopefolioImage} alt="Descripción del primer proyecto" className="project-image" />
+        </div>
+        <div className="project-text-container">
+          <h1 className="project-title">Portafolio</h1>
+          <p className="project-description">
+            Explora una selección de mis proyectos más destacados. Cada proyecto refleja mi experiencia y habilidades en desarrollo web y diseño. Desde soluciones innovadoras hasta implementaciones eficientes, aquí encontrarás ejemplos de mi trabajo en acción.
+          </p>
+          <button className="project-button" onClick={handleButtonClick}>
+            Mi Proyecto
+          </button>
+        </div>
       </div>
-      <div className="projects-text-container">
-        <h1 className="projects-title">Título del Proyecto</h1>
-        <p className="projects-description">
-          Aquí va la descripción del producto. Puedes incluir información relevante y detalles que quieras destacar.
-        </p>
+
+      {/* Segundo proyecto - Imagen a la derecha */}
+      <div className="project-container right">
+        <div className="project-text-container">
+          <h1 className="project-title">Otro Proyecto</h1>
+          <p className="project-description">
+            Este proyecto muestra un enfoque diferente en el diseño y desarrollo. La combinación de creatividad y tecnología se refleja en cada detalle, ofreciendo una experiencia de usuario única y efectiva.
+          </p>
+          <button className="project-button" onClick={handleButtonClick}>
+            Mi Proyecto
+          </button>
+        </div>
+        <div className="project-image-container">
+          <img src={wilsonportImage} alt="Descripción del segundo proyecto" className="project-image" />
+        </div>
       </div>
     </section>
   );
